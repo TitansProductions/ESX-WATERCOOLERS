dab182039bfe451d422320bbcece18c637719de0
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2026-03-29 11:51:53 +0300

    Update client.lua

--- esx_tgo_watercoolers/client.lua
+++ esx_tgo_watercoolers/client.lua
@@ -1,39 +1,48 @@
-ESX = nil
+local ESX = nil
 
 local waterCoolers = {-742198632}
-local IsAnimated = false
+local IS_ANIMATED = false
 
 Citizen.CreateThread(function()
     while ESX == nil do
         TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
+        Citizen.Wait(1000)
     end
 end)
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(5)
-        if not IsAnimated then
-            local ped = GetPlayerPed(-1)
-            local pos = GetEntityCoords(ped)
+
+        Wait(1)
+
+        local sleep  = 1200
+        local coords = GetEntityCoords(PlayerPedId())
+
+        if IS_ANIMATED then
+            goto END
+        end
+        
+        if not IS_ANIMATED then
 
             for i = 1, #waterCoolers do
-                local watercooler = GetClosestObjectOfType(pos.x, pos.y, pos.z, 1.0, waterCoolers[i], false, false, false)
-                local waterCoolerPos = GetEntityCoords(watercooler)
-                local dist = GetDistanceBetweenCoords(pos.x, pos.y, pos.z, waterCoolerPos.x, waterCoolerPos.y, waterCoolerPos.z, true)
 
+                local watercooler       = GetClosestObjectOfType(coords.x, coords.y, coords.z, 1.0, waterCoolers[i], false, false, false)
+                local waterCoolercoords = GetEntityCoords(watercooler)
+                local dist              = GetDistanceBetweenCoords(coords.x, coords.y, coords.z, waterCoolercoords.x, waterCoolercoords.y, waterCoolercoords.z, true)
+    
                 if dist < 1.8 then
-                    local loc = vector3(waterCoolerPos.x, waterCoolerPos.y, waterCoolerPos.z + 1.0)
-                    
-                    ESX.Game.Utils.DrawText3D(loc, 'Πατήστε [~y~E~w~] για να πείτε νερό.', 0.7)
+                    sleep = 0
+
+                    ESX.Game.Utils.DrawText3D(vector3(waterCoolercoords.x, waterCoolercoords.y, waterCoolercoords.z + 1.0), 'Press [~y~E~w~] to drink water.', 0.7)
+
                     if IsControlJustReleased(0, 38) then
-
-                        if not IsAnimated then
+    
+                        if not IS_ANIMATED then
                             prop_name = prop_name or 'prop_cs_paper_cup'
-                            IsAnimated = true
-
+                            IS_ANIMATED = true
+    
                             TriggerServerEvent('esx_tgo_watercoolers:refillThirst')
-
+    
                             Citizen.CreateThread(function()
                                 local playerPed = PlayerPedId()
                                 local x,y,z = table.unpack(GetEntityCoords(playerPed))
@@ -43,19 +52,23 @@ Citizen.CreateThread(function()
                     
                                 ESX.Streaming.RequestAnimDict('mp_player_intdrink', function()
                                     TaskPlayAnim(playerPed, 'mp_player_intdrink', 'loop_bottle', 1.0, -1.0, 2000, 0, 1, true, true, true)
-
+    
                                     Citizen.Wait(3000)
-                                    IsAnimated = false
+                                    IS_ANIMATED = false
                                     ClearPedSecondaryTask(playerPed)
                                     DeleteObject(prop)
                                 end)
                             end)
                         end
                     end
-                else
-                    Citizen.Wait(1500)
+    
                 end
+
             end
+
         end
+
+        ::END::
+        Wait(sleep)
     end
 end)
